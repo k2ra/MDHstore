ebe07f815d42e9b3aac58db92e5418bc8548166e
kevin <kevin_rojas30@hotmail.com>
2018-05-17 00:32:15 -0500

    building the controller

--- core/controllers/registerController.php
+++ core/controllers/registerController.php
@@ -1,3 +1,24 @@
 <?php
-	include(HTML_DIR . 'register.php');
+	if(isset($_session['user'])){
+
+	}
+	else{
+
+		switch (isset($_GET['mode']) ? $_GET['mode'] : null) {
+			case 'add':
+				if(isset($_POST)){
+					include(HTML_DIR . 'inicio.php');
+				}else{
+					
+					include(HTML_DIR . 'register.php');
+				}
+				
+			break;
+			
+			default:
+				include(HTML_DIR . 'register.php');
+			break;
+		}
+		
+	}
 ?>
--- html/register.php
+++ html/register.php
@@ -24,16 +24,16 @@
 	<div class="container">
 		<h1>Register</h1>
 		<em></em>
-		<h2><a href="index.html">Home</a><label>/</label>Register</a></h2>
+		<h2><a href=".">Home</a><label>/</label>Register</a></h2>
 	</div>
 </div>
 <!--login-->
 <div class="container">
 		<div class="login">
-			<form>
-			<div class="col-md-6 login-do">
-			<div class="login-mail">
-					<input type="text" placeholder="Name" required="">
+			<form method="post" action="?view=register&mode=add" >
+				<div class="col-md-6 login-do">
+				<div class="login-mail">
+					<input type="text" placeholder="Name" required="" id="name">
 					<i  class="glyphicon glyphicon-user"></i>
 				</div>
 				<div class="login-mail">
@@ -48,24 +48,24 @@
 					<input type="password" placeholder="Password" required="">
 					<i class="glyphicon glyphicon-lock"></i>
 				</div>
-				   <a class="news-letter " href="#">
-						 <label class="checkbox1"><input type="checkbox" name="checkbox" ><i> </i>Forget Password</label>
-					   </a>
-				<label class="hvr-skew-backward">
-					<input type="submit" value="Submit">
-				</label>
-			
-			</div>
-			<div class="col-md-6 login-right">
-				 <h3>Completely Free Account</h3>
-				 
-				 <p>Pellentesque neque leo, dictum sit amet accumsan non, dignissim ac mauris. Mauris rhoncus, lectus tincidunt tempus aliquam, odio 
-				 libero tincidunt metus, sed euismod elit enim ut mi. Nulla porttitor et dolor sed condimentum. Praesent porttitor lorem dui, in pulvinar enim rhoncus vitae. Curabitur tincidunt, turpis ac lobortis hendrerit, ex elit vestibulum est, at faucibus erat ligula non neque.</p>
-				<a href="?view=login" class="hvr-skew-backward">Login</a>
+					   <a class="news-letter " href="#">
+							 <label class="checkbox1"><input type="checkbox" name="checkbox" ><i> </i>Forget Password</label>
+						   </a>
+					<label class="hvr-skew-backward">
+						<input type="submit" value="Submit">
+					</label>
+				
+					</div>
+					<div class="col-md-6 login-right">
+					 <h3>Completely Free Account</h3>
+					 
+					 <p>Pellentesque neque leo, dictum sit amet accumsan non, dignissim ac mauris. Mauris rhoncus, lectus tincidunt tempus aliquam, odio 
+					 libero tincidunt metus, sed euismod elit enim ut mi. Nulla porttitor et dolor sed condimentum. Praesent porttitor lorem dui, in pulvinar enim rhoncus vitae. Curabitur tincidunt, turpis ac lobortis hendrerit, ex elit vestibulum est, at faucibus erat ligula non neque.</p>
+					<a href="?view=login" class="hvr-skew-backward">Login</a>
 
-			</div>
-			
-			<div class="clearfix"> </div>
+					</div>
+				
+					<div class="clearfix"> </div>
 			</form>
 		</div>
 
